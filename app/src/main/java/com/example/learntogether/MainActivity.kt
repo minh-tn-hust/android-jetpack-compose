b12de4da83be6acc5c36e274b388e6cd841927f6
minh-tn-hust <minh.tn.hust@gmail.com>
2022-10-23 01:39:20 +0700

    Task manager

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.learntogether
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +31,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colors.background,
                 ) {
-                    Greeting("Android")
+                    TaskManager()
                 }
             }
         }
@@ -30,8 +41,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun FinishImage() {
+    val finishedImage = painterResource(id = R.drawable.ic_launcher_background)
+    Image(
+        painter = finishedImage,
+        contentDescription = null,
+        contentScale = ContentScale.Fit
+    )
+}
+@Composable
+fun TaskManager() {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxHeight().wrapContentHeight(align = Alignment.CenterVertically)
+
+    ) {
+        FinishImage()
+        Text(
+            text = "All tasks completed",
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.W500,
+        )
+        Text(
+            text = "Nice work!",
+            fontSize = 16.sp
+        )
+    }
 }
 
 @Preview(
@@ -39,8 +75,8 @@ fun Greeting(name: String) {
     showSystemUi = true
 )
 @Composable
-fun DefaultPreview() {
+fun TaskManagerPreview() {
     LearnTogetherTheme {
-        Greeting("Android")
+        TaskManager()
     }
 }
