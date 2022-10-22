a8a527350f981e028f87a0a1296edeac61d2596a
minh-tn-hust <minh.tn.hust@gmail.com>
2022-10-23 01:20:13 +0700

    Learn Together App

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.learntogether
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    LearnTogetherLayout()
                 }
             }
         }
@@ -30,8 +42,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TopImage() {
+    val topImage = painterResource(id = R.drawable.bg_compose_background)
+    Image(
+        painter = topImage,
+        contentDescription = null,
+        modifier = Modifier.fillMaxWidth(),
+        contentScale = ContentScale.Fit
+    )
+}
+
+@Composable
+fun Title(title: String) {
+    Text(
+        text = title,
+        modifier = Modifier.padding(all = 16.dp),
+        fontSize = 24.sp
+    )
+}
+
+@Composable
+fun Content() {
+    Column() {
+        Text(
+            text = stringResource(id = R.string.learn_together_1st_para),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.learn_together_2st_para),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(all = 16.dp)
+        )
+    }
+}
+
+@Composable
+fun LearnTogetherLayout() {
+    Column {
+        TopImage()
+        Title(title = stringResource(R.string.learn_together_title))
+        Content()
+    }
 }
 
 @Preview(
@@ -39,8 +91,8 @@ fun Greeting(name: String) {
     showSystemUi = true
 )
 @Composable
-fun DefaultPreview() {
+fun LearnTogetherLayoutPreview() {
     LearnTogetherTheme {
-        Greeting("Android")
+        LearnTogetherLayout()
     }
 }
