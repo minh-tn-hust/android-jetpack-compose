cdbf4854103710867e8bc37c1152fdeed1140f47
minh-tn-hust <minh.tn.hust@gmail.com>
2022-10-23 23:08:16 +0700

    bug: Last row with 1/4 max width

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.learntogether
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ComposeQuadrant()
                 }
             }
         }
@@ -30,8 +38,64 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Quadrant(title: String, content: String, colorBackground: Color) {
+    Box(
+        modifier = Modifier.background(colorBackground).fillMaxWidth(fraction = 0.5f).fillMaxHeight()
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(all = 16.dp).fillMaxHeight().fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = title,
+                fontWeight = FontWeight.W500
+            )
+            Text(
+                text = content,
+                textAlign = TextAlign.Justify
+            )
+        }
+    }
+
+}
+
+@Composable
+fun ComposeQuadrant() {
+    Column(
+        modifier = Modifier.fillMaxWidth().fillMaxHeight().background(Color.Magenta)
+    ) {
+        Row(
+            modifier = Modifier.weight(weight = 1f, fill = true).fillMaxHeight()
+        ) {
+            Quadrant(
+                title = stringResource(R.string.quadrant1st_title),
+                content = stringResource(R.string.quadrant1st_content),
+                colorBackground = Color.Green
+            )
+            Quadrant(
+                title = stringResource(R.string.quadrant2nd_title),
+                content = stringResource(R.string.quadrant2nd_content),
+                colorBackground = Color.Yellow
+            )
+        }
+        Row(
+            modifier = Modifier.weight(weight = 1f, fill = true).fillMaxHeight()
+        ) {
+            Quadrant(
+                title = stringResource(R.string.quadrant3rd_title),
+                content = stringResource(R.string.quadrant3rd_content),
+                colorBackground = Color.Cyan,
+            )
+            Quadrant(
+                title = stringResource(R.string.quadrant4th_title),
+                content = stringResource(R.string.quadrant4th_content),
+                colorBackground = Color.LightGray
+            )
+        }
+    }
+
 }
 
 @Preview(
@@ -39,8 +103,8 @@ fun Greeting(name: String) {
     showSystemUi = true
 )
 @Composable
-fun DefaultPreview() {
+fun ComposeQuadrantPreview() {
     LearnTogetherTheme {
-        Greeting("Android")
+        ComposeQuadrant()
     }
 }
